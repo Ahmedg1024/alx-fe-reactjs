c40ce0a75b0c24e80e1a8472d0e43dd14a0ae36e
Ahmed Gamal <ahmedg1024@gmail.com>
2024-11-02 23:17:15 +0300

    adit bio to Bio

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile (props) {
     return(<>
         <h2>{props.name}</h2>
         <p>Age: {props.age}</p>
-        <p>bio: {props.bio}</p>    
+        <p>Bio: {props.bio}</p>    
     </>
     );
 }
