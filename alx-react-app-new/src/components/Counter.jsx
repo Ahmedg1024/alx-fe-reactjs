2ddebd65923d365d4e6762d8fe8897c87c83b80d
Ahmed Gamal <ahmedg1024@gmail.com>
2024-11-10 22:39:42 +0200

    edit some shape of the function

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
 
-function 
-Counter() {
+function Counter() {
 const [count,setcount]=useState(0)
 
   return (
-    <>
+    <div>
     <p>Current Count: {count}</p>
-    <button onClick={()=>setcount(count+1)}>increment</button>
-    <button onClick={()=>setcount(count-1)}>decrement</button>
+    <button onClick={()=>setcount(count+1)}>Increment</button>
+    <button onClick={()=>setcount(count-1)}>Decrement</button>
     <button onClick={()=>setcount(0)}>Reset</button>
-    </>
+    </div>
   );
 }
 
