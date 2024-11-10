8919e1dfd42a96c20c3bb2c2df474a0c9d0f1c8f
Ahmed Gamal <ahmedg1024@gmail.com>
2024-11-10 22:43:36 +0200

    setCount changed to camel case writing

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 
 function Counter() {
-const [count,"setcount"]=useState(0)
+const [count,setCount]=useState(0)
 
   return (
     <div>
     <p>Current Count: {count}</p>
-    <button onClick={()=>setcount(count+1)}>Increment</button>
-    <button onClick={()=>setcount(count-1)}>Decrement</button>
-    <button onClick={()=>setcount(0)}>Reset</button>
+    <button onClick={()=>setCount(count+1)}>Increment</button>
+    <button onClick={()=>setCount(count-1)}>Decrement</button>
+    <button onClick={()=>setCount(0)}>Reset</button>
     </div>
   );
 }
