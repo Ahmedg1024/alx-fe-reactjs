daa611b306a1a85a2c0a22744e18446b33e3a830
Ahmed Gamal <ahmedg1024@gmail.com>
2024-11-10 23:51:05 +0200

    added multiple files to use context

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+import UserDetails from "./UserDetails";
+
+function ProfilePage(){
+    return(
+        <UserDetails />
+    )
+}
+export default ProfilePage
