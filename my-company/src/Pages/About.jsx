4d6f5053c0b4b63a1c84d365fb6c97ce51caf1d4
Ahmed Gamal <ahmedg1024@gmail.com>
2024-11-11 23:36:51 +0200

    task finished by adding content to all pages

--- my-company/src/Pages/About.jsx
+++ my-company/src/Pages/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+       <h1>About Us</h1>
+       <p>Our company has been providing top-notch services since 1990. We specialize in various fields including technology, marketing, and consultancy.</p>
+    </div>
   )
 }
 
