d4e65a259cae31c312103e0e07dbf2da025ec418
Ahmed Gamal <ahmedg1024@gmail.com>
2024-11-11 23:50:56 +0200

    add some styling to Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav style={{textAlign:'center',padding:'3px 5px'}}>
+    <nav style={{backgroundColor:'yellow', display:'flex',justifyContent:'center'}}>
 
-    <NavLink style={{ textDecoration:'none', color:'red'}} to="/">Home_Page</NavLink>
+    <NavLink style={{ textDecoration:'none', color:'red',margin:'25px'}} to="/">Home_Page</NavLink>
     <br/>
-    <NavLink style={{ textDecoration:'none', color:'Gray'}} to="/About">About_Page</NavLink> 
+    <NavLink style={{ textDecoration:'none', color:'Gray',margin:'25px'}} to="/About">About_Page</NavLink> 
     <br/>
-    <NavLink style={{ textDecoration:'none', color:'blue'}} to='/Services'>Services_Page</NavLink> 
+    <NavLink style={{ textDecoration:'none', color:'blue',margin:'25px'}} to='/Services'>Services_Page</NavLink> 
     <br/>
-    <NavLink style={{ textDecoration:'none', color:'yellow'}} to='/Contact'>Contact_Page</NavLink> 
+    <NavLink style={{ textDecoration:'none', color:'green',margin:'25px'}} to='/Contact'>Contact_Page</NavLink> 
     
     </nav>
   );
